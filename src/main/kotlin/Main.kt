a872e5da2c16f75a550314dcd506603d264310af
Taekyeong Yoo <dev.tkyoo@gmail.com>
2022-10-09 14:02:21 +0900

    소수인지 판별하는 함수 추가
    - 주어진 수 보다 작거타 같은 자연수 중 3 이상의 홀수로 주어진 수에 대해 나머지 연산을 수행한다.
    - 자연수 2보다 작은 수가 주어지는 경우 false 를 반환한다.
    - 2가 주어지는 경우 true 를 반환한다.
    - 2보다 큰 모든 짝수는 false 를 반환한다.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,30 @@
 fun main(args: Array<String>) {
     println("Hello World!")
 
+    val number = 101
+    println(isPrime(number))
+
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+}
+
+// 1보다 큰 수 중에서 1과 자기 자신으로만 나누어 떨어지는 수
+fun isPrime(number: Int): Boolean {
+    if (number < 2)
+        return false
+    else if (number == 2)
+        return true
+    else if (number % 2 == 0)
+        return false
+    else
+        for (i in 3..number step(2)) {
+            println("$number % $i is " + number % i)
+            if (number % i == 0 && number != i)
+                return false
+            if (number % i == 0 && number == i)
+                return true
+        }
+
+    return false
 }
